bc3c71d1626aa04a8c23e41a84703f0f414306a7
Emil Borowiec <emil@ponderingprogrammer.com>
2020-09-23 21:42:50 +0200

    Added user name field for registration form.

--- Dajumbl/src/PonderingProgrammer.Dajumble.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Dajumbl/src/PonderingProgrammer.Dajumble.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,6 +46,11 @@ namespace PonderingProgrammer.Dajumble.Web.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [StringLength(39, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+            [Display(Name = "User name")]
+            public string UserName { get; set; }
+            
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -74,7 +79,7 @@ namespace PonderingProgrammer.Dajumble.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
