90d4758e6bc66d24f4863295cdca1be524f6af90
Emil Borowiec <emil@ponderingprogrammer.com>
2020-09-24 14:19:09 +0200

    List of owned contexts on dashboard

--- Dajumbl/src/PonderingProgrammer.Dajumble.Web/Pages/UserDashboard.cshtml.cs
+++ Dajumbl/src/PonderingProgrammer.Dajumble.Web/Pages/UserDashboard.cshtml.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PonderingProgrammer.Dajumble.Web.Data;
+using PonderingProgrammer.Dajumble.Web.Model;
 
 namespace PonderingProgrammer.Dajumble.Web.Pages
 {
     public class UserDashboardModel : PageModel
     {
+        private readonly ContextRepository _contextRepository;
+
+        public UserDashboardModel(ContextRepository contextRepository)
+        {
+            _contextRepository = contextRepository ?? throw new ArgumentNullException(nameof(contextRepository));
+        }
+
         public string UserName { get; set; }
+        public List<Context> Contexts { get; set; }
         
         public void OnGet()
         {
             UserName = User.Identity.Name;
+            Contexts = _contextRepository.FindUserOwnedContexts(User.GetUserId());
         }
     }
 }
