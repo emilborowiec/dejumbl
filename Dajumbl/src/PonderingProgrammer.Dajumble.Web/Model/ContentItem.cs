a95892e21d900c795f73499df3d70af9144b6d7b
Emil Borowiec <emil@ponderingprogrammer.com>
2020-09-25 17:56:20 +0200

    DB model fix. Showing list of items in context details.

--- Dajumbl/src/PonderingProgrammer.Dajumble.Web/Model/ContentItem.cs
+++ Dajumbl/src/PonderingProgrammer.Dajumble.Web/Model/ContentItem.cs
@@ -10,7 +10,7 @@ namespace PonderingProgrammer.Dajumble.Web.Model
         }
 
         public string Id { get; private set; }
-        public ContentItemType Item { get; set; }
+        public ContentItemType ItemType { get; set; }
         public string Label { get; set; }
         public string Content { get; set; }
     }
