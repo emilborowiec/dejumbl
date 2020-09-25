2a366ea8c316eeefab8b39901d930ea0924e6f44
Emil Borowiec <emil@ponderingprogrammer.com>
2020-09-25 08:51:22 +0200

    Added owner user name to context page.

--- Dajumbl/src/PonderingProgrammer.Dajumble.Web/Pages/Context.cshtml.cs
+++ Dajumbl/src/PonderingProgrammer.Dajumble.Web/Pages/Context.cshtml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PonderingProgrammer.Dajumble.Web.Data;
@@ -9,6 +11,7 @@ namespace PonderingProgrammer.Dajumble.Web.Pages
     public class ContextModel : PageModel
     {
         private readonly ContextRepository _repository;
+        private readonly UserManager<IdentityUser> _userManager;
 
         public ContextModel(ContextRepository repository)
         {
@@ -16,8 +19,10 @@ namespace PonderingProgrammer.Dajumble.Web.Pages
         }
 
         public Context Context { get; set; }
+        public bool OwnedByCurrentUser { get; set; }
+        public string OwnerUserName { get; set; }
 
-        public IActionResult OnGet(string id)
+        public async Task<IActionResult> OnGetAsync(string id)
         {
             var context = _repository.FindById(id);
             if (context == null)
@@ -26,6 +31,16 @@ namespace PonderingProgrammer.Dajumble.Web.Pages
             }
 
             Context = context;
+            OwnedByCurrentUser = context.OwnerUserId == User.GetUserId(); 
+            if (OwnedByCurrentUser)
+            {
+                OwnerUserName = User.Identity.Name;
+            }
+            else
+            {
+                var ownerUser = await _userManager.FindByIdAsync(context.OwnerUserId);
+                OwnerUserName = ownerUser?.UserName ?? "unknown user";
+            }
             
             return Page();
         }
