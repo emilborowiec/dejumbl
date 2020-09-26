00eea39f2788365726e998905421eabf31fc9e98
Emil Borowiec <emil@ponderingprogrammer.com>
2020-09-26 11:36:34 +0200

    Relation types

---
+++ Dajumbl/src/PonderingProgrammer.Dajumble.Web/Model/RelationType.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PonderingProgrammer.Dajumble.Web.Model
+{
+    public enum RelationType
+    {
+        [Display(Name = "Is related to", Description = "Most generic type of relation.")]
+        IsRelatedTo,
+        [Display(Name = "Is analogous to", Description = "Represents same concept but in different environment.")]
+        IsAnalogousTo,
+        [Display(Description = "Abstraction is a simplified concept with removed details.")]
+        Abstracts,
+        [Display(Description = "Generalization is a more general concept formulated from more specific instances and based on their common properties.")]
+        Instantiates,
+        Generalizes,
+        Exemplifies,
+        [Display(Description = "Group concepts, broad concepts or complex concepts can include its constituents.")]
+        Includes,
+        [Display(Name = "Is part of")]
+        IsPartOf,
+        Supports,
+        [Display(Name = "Is supported by")]
+        IsSupportedBy,
+        Contradicts,
+        [Display(Name = "Is contradicted by")]
+        IsContradictedBy,
+    }
+}
